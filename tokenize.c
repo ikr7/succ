2ce3f96ea4b611481980e6704f3b76032af57e3b
ikr7 <kunpei.ikuta@gmail.com>
2021-01-17 03:01:33 +0900

    split files

---
+++ tokenize.c
@@ -0,0 +1,104 @@
+#include "9cc.h"
+
+void error_at(char* loc, char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+bool consume(char* op) {
+    if (
+        token->kind != TK_RESERVED ||
+        strlen(op) != token->len ||
+        strncmp(token->str, op, token->len) != 0
+    ) {
+        return false;
+    }
+    token = token-> next;
+    return true;
+}
+
+void expect(char* op) {
+    if (
+        token->kind != TK_RESERVED ||
+        strlen(op) != token->len ||
+        strncmp(token->str, op, token->len) != 0
+    ) {
+        error_at(token->str, "not '%c'", op);
+    }
+    token = token-> next;
+}
+
+int expect_number() {
+    if (token->kind != TK_NUM) {
+        error_at(token->str, "Not a number");
+    }
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof() {
+    return token->kind == TK_EOF;
+}
+
+Token* new_token(TokenKind kind, Token* cur, char* str, int len) {
+    Token* tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    tok->len = len;
+    cur->next = tok;
+    return tok;
+}
+
+Token* tokenize() {
+    char* p = user_input;
+    Token head;
+    head.next = NULL;
+    Token* cur = &head;
+    while (*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+        if (
+            strncmp(p, "==", 2) == 0 ||
+            strncmp(p, "!=", 2) == 0 ||
+            strncmp(p, "<=", 2) == 0 ||
+            strncmp(p, ">=", 2) == 0
+        ) {
+            cur = new_token(TK_RESERVED, cur, p, 2);
+            p += 2;
+            continue;
+        }
+        if (
+            *p == '+' ||
+            *p == '-' ||
+            *p == '*' ||
+            *p == '/' ||
+            *p == '(' ||
+            *p == ')' ||
+            *p == '<' ||
+            *p == '>'
+        ) {
+            cur = new_token(TK_RESERVED, cur, p++, 1);
+            continue;
+        }
+        if (isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p, 0);
+            char *q = p;
+            cur->val = strtol(p, &p, 10);
+            cur->len = p - q;
+            continue;
+        }
+        error_at(p, "Cannot be tokenized");
+    }
+    new_token(TK_EOF, cur, p, 0);
+    return head.next;
+}
